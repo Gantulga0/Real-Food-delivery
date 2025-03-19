ac5f16350a4b2e0150866948115cbdf9423537e7
24LP9558 24LP9558 <tudewdagwagantulga@gmail.com>
2025-03-19 11:54:36 +0800

    small fix

--- BackEnd/src/resolvers/food/getFood.js
+++ BackEnd/src/resolvers/food/getFood.js
@@ -2,22 +2,14 @@ import { Food } from '../../models/food.schema.js';
 
 export const getFood = async (req, res) => {
   try {
-    const { categoryId } = req.query; // Extract categoryId from query params
+    const { categoryId } = req.query;
 
-    // If a categoryId is provided, filter foods by that category
-    const query = categoryId ? { category: categoryId } : {}; // Filter only if categoryId is provided
+    const query = categoryId ? { category: categoryId } : {};
 
-    // Fetch foods based on the query
-    const foods = await Food.find(query).populate('category'); // Populate the category field
-
-    if (!foods || foods.length === 0) {
-      return res.status(404).send({ message: 'No foods found' });
-    }
+    const foods = await Food.find(query).populate('category');
 
     res.send({ message: 'Foods retrieved successfully', foods });
   } catch (error) {
-    res
-      .status(500)
-      .send({ message: 'Error retrieving foods', error: error.message });
+    res.status(500).send({ message: 'Error retrieving foods', error: error.message });
   }
 };
